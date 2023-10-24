ddc0cec0f5883c3e126048a2be8c17fcfd502d10
markwoerdenweber <116349750+markwoerdenweber@users.noreply.github.com>
2023-10-24 15:26:39 +0200

    Create script.sh

---
+++ .github/script.sh
@@ -0,0 +1,13 @@
+BRANCH_NAME_LINUX=$GITHUB_REF_NAME
+BRANCH_NAME_WINDOWS=$env:GITHUB_REF_NAME
+
+# Take the non-empty name.
+str=${BRANCH_NAME_LINUX:-BRANCH_NAME_WINDOWS}
+
+# Replace trailing variable versions.
+find=".x"
+replace=""
+BRANCH_NAME=${str//$find/$replace}
+
+# Set as output.
+echo "branch-label=${BRANCH_NAME}" >> $GITHUB_OUTPUT
