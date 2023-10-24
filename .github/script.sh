d33868f45abfc79f0975aee01aa784399701244d
markwoerdenweber <116349750+markwoerdenweber@users.noreply.github.com>
2023-10-24 15:18:01 +0200

    Update script.sh

--- .github/script.sh
+++ .github/script.sh
@@ -1,4 +1,16 @@
-str=${$GITHUB_REF_NAME:-$env:GITHUB_REF_NAME}
+BRANCH_NAME_LINUX=$GITHUB_REF_NAME
+BRANCH_NAME_WINDOWS=$env:GITHUB_REF_NAME
+
+# Take the non-empty name.
+str=${BRANCH_NAME_LINUX:-BRANCH_NAME_WINDOWS}
+
+# Replace trailing variable versions.
 find=".x"
 replace=""
-echo "branch-label=${str//$find/$replace}" >> $GITHUB_OUTPUT
+BRANCH_NAME=${str//$find/$replace}
+
+# Send as output.
+echo "branch-label=${BRANCH_NAME}" >> $GITHUB_OUTPUT
+
+# For testing.
+echo "1.2.3"
