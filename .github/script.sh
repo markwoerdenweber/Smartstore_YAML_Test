be5f69dc8b050c52d23fdd1c78f752f075080828
markwoerdenweber <116349750+markwoerdenweber@users.noreply.github.com>
2023-10-25 09:23:29 +0200

    Update script.sh

--- .github/script.sh
+++ .github/script.sh
@@ -1,13 +1,12 @@
+# Get the Linux and Windows branch name from the GitHub action's input
 BRANCH_NAME_LINUX=$GITHUB_REF_NAME
 BRANCH_NAME_WINDOWS=$env:GITHUB_REF_NAME
 
-# Take the non-empty name.
-str=${BRANCH_NAME_LINUX:-BRANCH_NAME_WINDOWS}
+# Use the Windows branch name if the linux branch name is unset
+BRANCH_NAME=${BRANCH_NAME_LINUX:-BRANCH_NAME_WINDOWS}
 
-# Replace trailing variable versions.
-find=".x"
-replace=""
-BRANCH_NAME=${str//$find/$replace}
+# Remove '.x' from the end of the branch name.
+BRANCH_NAME=${BRANCH_NAME%.x}
 
-# Set as output.
+# Set the output 'branch-label' to the branch name.
 echo "branch-label=${BRANCH_NAME}" >> $GITHUB_OUTPUT
